19af47b133843a2923f74dd0a7169a2a5ec1605f
aditvaidya1 <79274027+aditvaidya1@users.noreply.github.com>
2023-01-17 10:33:45 -0500

    Add files via upload

---
+++ login.js
@@ -0,0 +1,19 @@
+var v3="123";
+var v4="123";
+function func1()
+{
+var v1, v2;
+v1 = document.getElementById("username").value; 
+v2 = document.getElementById("password").value;
+if ((v1 == v3) &&
+   (v2 ==  v4))
+{
+    alert('hi');
+    window.open("index.html");
+}
+else
+{    
+    alert("Enter Adit@proj.ca & 123");
+    
+}
+}
